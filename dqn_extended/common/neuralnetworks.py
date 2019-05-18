860886844838660a068907b4bd14ab49542a6d60
Anaelle Laurans <16943368+Laurans@users.noreply.github.com>
2019-05-18 15:15:19 +0200

    :alembic: Try divide by 256

--- dqn_extended/common/neuralnetworks.py
+++ dqn_extended/common/neuralnetworks.py
@@ -28,6 +28,6 @@ class DQN(nn.Module):
         return int(np.prod(o.size()))
 
     def forward(self, x):
-        fx = x.float() / 255
+        fx = x.float() / 256.0
         conv_out = self.conv(fx).view(fx.size()[0], -1)
         return self.fc(conv_out)

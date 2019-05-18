71adad61ccffba481a144cd7c93795995b9be2f0
Anaelle Laurans <16943368+Laurans@users.noreply.github.com>
2019-05-18 11:41:37 +0200

    :sparkles: Add config params as yaml file

---
+++ DQN_extended/common/configreader.py
@@ -0,0 +1,28 @@
+from yaml import SafeLoader, load
+import re
+
+
+def read_yaml(filename: str):
+    loader = SafeLoader
+    loader.add_implicit_resolver(
+        u"tag:yaml.org,2002:float",
+        re.compile(
+            u"""^(?:
+        [-+]?(?:[0-9][0-9_]*)\\.[0-9_]*(?:[eE][-+]?[0-9]+)?
+        |[-+]?(?:[0-9][0-9_]*)(?:[eE][-+]?[0-9]+)
+        |\\.[0-9_]+(?:[eE][-+][0-9]+)?
+        |[-+]?[0-9][0-9_]*(?::[0-5]?[0-9])+\\.[0-9_]*
+        |[-+]?\\.(?:inf|Inf|INF)
+        |\\.(?:nan|NaN|NAN))$""",
+            re.X,
+        ),
+        list(u"-+0123456789."),
+    )
+    with open(filename, "r") as f:
+        config = load(f, Loader=loader)
+    return config
+
+
+def get_config():
+    return read_yaml("config/hyperparams.yml")
+

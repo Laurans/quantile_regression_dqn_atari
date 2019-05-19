fff9e481672a2a2e94254985fb35036f64e7247c
Anaelle Laurans <16943368+Laurans@users.noreply.github.com>
2019-05-19 14:01:19 +0200

    :sparkles: Add tensorboardx + offline log

--- dqn_extended/05_rainbow_without_prio.py
+++ dqn_extended/05_rainbow_without_prio.py
@@ -1,22 +1,44 @@
+import os
+
 import click
 import gym
 import ptan
-
 import torch
 import torch.optim as optim
+import wandb
+from tensorboardX import SummaryWriter
 from torch.nn.utils import clip_grad_norm_
 
-from dqn_extended.common import configreader, neuralnetworks, trackers, losses
-import wandb
-import objgraph
+from dqn_extended.common import configreader, losses, neuralnetworks, trackers
 
 torch.backends.cudnn.benchmark = True
 
+os.environ["WANDB_MODE"] = "dryrun"
+
 
 def init_logger(params):
-    wandb.init(
-        name="dqn_classic_d", project="dqn_extended", dir="../wandb", config=params
-    )
+    project = "dqn_extended"
+    name = "dqn_double"
+    logs_dir = "../logs"
+    wandb.init(name=name, project=project, dir=logs_dir, config=params)
+
+    writer = SummaryWriter(logs_dir + "/" + name)
+    return writer
+
+
+def write_log(writer: SummaryWriter, scalars_dict: dict, step):
+    for key, value in scalars_dict.items():
+        writer.add_scalar(key, value, global_step=step)
+
+    wandb.log(scalars_dict, step=step)
+
+
+def watch_model(
+    writer: SummaryWriter, model: torch.nn.Module, dummy_input_shape: tuple
+):
+    dummy_input = (torch.Tensor(1, *dummy_input_shape),)
+    writer.add_graph(model, dummy_input, True)
+    wandb.watch(model, log=None)
 
 
 @click.command()
@@ -25,15 +47,15 @@ def main(gpu):
     params = configreader.get_config("./common/config/hyperparams.yaml")["pong"]
     params["device"] = f"cuda:{gpu}"
     params["batch_size"] *= params["train_freq"]
-    init_logger(params)
+    writer = init_logger(params)
 
     env = gym.make(params["env_name"])
     env = ptan.common.wrappers.wrap_dqn(env)
 
-    net = neuralnetworks.DuelingDQN(env.observation_space.shape, env.action_space.n)
+    net = neuralnetworks.DQN(env.observation_space.shape, env.action_space.n)
     net = net.to(params["device"])
 
-    wandb.watch(net, log=None)
+    watch_model(writer, net, env.observation_space.shape)
 
     tgt_net = ptan.agent.TargetNet(net)
     selector = ptan.actions.EpsilonGreedyActionSelector(epsilon=params["epsilon_start"])
@@ -71,10 +93,10 @@ def main(gpu):
                     logs["loss"] = loss_in_float
 
                 if i_episode == 0:
-                    wandb.log(logs, step=frame_idx)
+                    write_log(writer, logs, frame_idx)
 
                 if success:
-                    wandb.log(logs, step=frame_idx)
+                    write_log(writer, logs, frame_idx)
                     break
 
             if len(buffer) < params["replay_initial"]:

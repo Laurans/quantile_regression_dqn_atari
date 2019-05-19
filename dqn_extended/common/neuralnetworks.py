be9a8b7f26b24a86a469915de4d307a38ac261e3
Anaelle Laurans <16943368+Laurans@users.noreply.github.com>
2019-05-19 11:59:39 +0200

    :sparkles: Add dueling network

--- dqn_extended/common/neuralnetworks.py
+++ dqn_extended/common/neuralnetworks.py
@@ -35,6 +35,41 @@ class DQN(nn.Module):
         return self.fc(conv_out)
 
 
+class DuelingDQN(nn.Module):
+    def __init__(self, input_shape, n_actions):
+
+        super(DuelingDQN, self).__init__()
+
+        self.conv = nn.Sequential(
+            nn.Conv2d(input_shape[0], 32, kernel_size=8, stride=4),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1),
+            nn.ReLU(),
+        )
+
+        conv_out_size = self._get_conv_out(input_shape)
+        self.fc_adv = nn.Sequential(
+            nn.Linear(conv_out_size, 512), nn.ReLU(), nn.Linear(512, n_actions)
+        )
+
+        self.fc_val = nn.Sequential(
+            nn.Linear(conv_out_size, 512), nn.ReLU(), nn.Linear(512, 1)
+        )
+
+    def _get_conv_out(self, shape):
+        o = self.conv(torch.zeros(1, *shape))
+        return int(np.prod(o.size()))
+
+    def forward(self, x):
+        fx = x.float() / 256.0
+        conv_out = self.conv(fx).view(fx.size()[0], -1)
+        val = self.fc_val(conv_out)
+        adv = self.fc_adv(conv_out)
+        return val + adv - adv.mean()
+
+
 class NoisyLinear(nn.Linear):
     def __init__(self, in_features, out_features, sigma_init=0.017, bias=True):
         super(NoisyLinear, self).__init__(in_features, out_features, bias=bias)
